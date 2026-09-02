cf7b124c1bcfc111a09dfefc32f4c51ac40f2bad
Eric Wang <ericnotsame@gmail.com>
2026-09-02 21:46:59 +0800

    fix(client): bundle monaco editor locally

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as monaco from 'monaco-editor'
+import { loader } from '@monaco-editor/react'
 import './index.css'
 import App from './App.tsx'
+
+loader.config({ monaco })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
